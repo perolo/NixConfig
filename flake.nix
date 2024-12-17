47d9a3e8a47016ab546b1f498fc3088bb48ce117
Per Olofsson <per.e.olofsson@gmail.com>
2024-12-17 10:52:34 +0800

    new attempt flake

--- flake.nix
+++ flake.nix
@@ -28,5 +28,13 @@
       # Optionally use extraSpecialArgs
       # to pass through arguments to home.nix
     };
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };    
   };
 }
